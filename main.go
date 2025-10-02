ffa2a2a6a9e2339ff76633981cf0b520ad5ed230
A-maina <4lwachira@gmail.com>
2025-10-02 16:35:07 +0300

    adds code

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World! 👋 from Go API")
+}
+
+func main() {
+    http.HandleFunc("/hello", helloHandler)
+    fmt.Println("Server running on http://localhost:8080/hello")
+    http.ListenAndServe(":8080", nil)
+}
